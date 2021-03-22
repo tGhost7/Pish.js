9b2fa6335adcfee495f603ae4a43b9a851f30034
isimsizz94 <58332630+isimsizz94@users.noreply.github.com>
2021-03-22 19:25:30 +0500

    Add files via upload

---
+++ qFrame/q.js
@@ -0,0 +1,20 @@
+import { qFrameCollection } from "./qFrameCollection.js";
+
+
+function q(selector){
+    return new qFrameCollection(selector);
+}
+
+// q("div") -> qFrameCollection
+// q(document.querySelectorAll("div")) -> qFrameCollection
+// q("div").original -> original Array/nodeList
+// q("div").each(function(a,b,c){}) a:HTMLElement, b:index, c:original Array -> q("div")
+// q("div").text("hah") if arg !== null and undefined change text all divs --- else return first textContent
+// q("div"). on/off --- add/remove eventListeners
+// q("div").eq(number) -> choosed qFrameCollection
+
+
+
+export {
+    q
+}
---
+++ qFrame/qFrameCollection.js
@@ -0,0 +1,62 @@
+class qFrameCollection {
+    constructor(selector) {
+        if(typeof selector === "function")
+            return window.addEventListener("load", selector);
+
+        if(selector instanceof NodeList){
+            selector.forEach( (e,i) => this[i] = e );
+        } else if(selector instanceof Element){
+            this[0] = selector;
+        } else if(typeof selector === "string" && selector.includes("<")){
+            let elem = document.createElement("div");
+            elem.innerHTML = selector;
+            elem.childNodes.forEach( (e, i) => this[i] = e);
+        } else {
+            document.querySelectorAll(selector).forEach( (e,i) => this[i] = e);
+        }
+    }
+    get original(){
+        return Object.values(this).filter( e => e instanceof Element);
+    }
+    get length(){
+        return Object.keys(this).filter( e => !Number.isNaN(+e) ).length;
+    }
+    each(fn){
+        let i = 0;
+        let original = this.original;
+        for(let el of original){
+            fn(el, i, original);
+            i++;
+        }
+        return this;
+    }
+    text(text){
+        if(text !== undefined && text !== null){
+           for(let el of this.original){
+               el.textContent = text;
+           }
+           return this;
+        }
+        return this[0].textContent;
+    }
+    on(type, listener, options){
+        for(let el of this.original){
+            el.addEventListener(type, listener, options)
+        }
+        return this;
+    }
+    off(type, listener, options){
+        for(let el of this.original){
+            el.removeEventListener(type, listener, options)
+        }
+        return this;
+    }
+    eq(index){
+        if(index < 0) index += this.length;
+        return new qFrameCollection(this[index]);
+    }
+}
+export {
+    qFrameCollection
+};
+
